4de7d76df9c58abf8263597402774cd7719f4d5b
beije-13 <beije9462@gmail.com>
2023-10-12 13:30:34 +0300

    update from googletrans to translators lib

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from typing import Text
 import telebot
 from telebot import types
-from googletrans import Translator
+import translators as ts
 
 with open('token.bot', 'r') as file:
     BOT_TOKEN = file.read()
 
 
-translator = Translator()
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN, skip_pending=True)
 
 
 @bot.inline_handler(lambda query: len(query.query) > 0)
@@ -16,11 +14,10 @@ def query_text(inline_query):
     translation = types.InlineQueryResultArticle(
         '1',
         'Translation',
-        types.InputTextMessageContent(f'{translator.translate(inline_query.query, dest="ru").text}'),
-        description=f'{translator.translate(inline_query.query, dest="ru").text}'
+        types.InputTextMessageContent(f'{ts.translate_text(inline_query.query, to_language="ru")}'),
+        description=f'{ts.translate_text(inline_query.query, to_language="ru")}'
     )
     bot.answer_inline_query(inline_query.id, [translation])
-
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
@@ -30,12 +27,11 @@ def start_command(message):
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
-    bot.send_message(message.from_user.id, 'I\'m inline bot. For translating text you need: \n-@ me in any chat you need\n-Write any text\n-Press the pop-up result to send translation to this chat.')
+    bot.reply_to(message, 'I\'m inline bot. For translating text you need: \n-@ me in any chat you need\n-Write any text\n-Press the pop-up result to send translation to this chat.')
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     bot.send_message(message.from_user.id, 'Type /help to get a quick guide :)')
 
-
-bot.polling(none_stop=True)
+bot.infinity_polling()
